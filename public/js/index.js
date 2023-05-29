999f3b53f8308c4c0f3004234107c5a9e0e9c859
AmanJadon2023 <aman.jadon@geminisolutions.com>
2023-05-29 10:30:09 +0530

    implemented functionality of sharing current location from one client to all using socket communcation and implementing chat functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,78 @@
+// import 'regenerator-runtime/runtime';
+// import axios from 'axios';
 var socket = io.connect();
-let counter = document.getElementById('counter1');
-let paragraph = document.getElementById('count');
-socket.on('countUpdated',(value)=>{
-    console.log('after updating',value)
-    paragraph.innerText = parseInt(value);
+// let counter = document.getElementById('counter1');
+// let paragraph = document.getElementById('count');
+// socket.on('countUpdated',(value)=>{
+//     console.log('after updating',value)
+//     paragraph.innerText = parseInt(value);
+// })
+// counter.addEventListener('click', () => {
+//     socket.emit('valueChange',parseInt(paragraph.innerText))
+// });
+
+socket.on('sendCityNameToAllClients', (message) => {
+    let messageTemplateHtml = document.getElementById('message-template').innerHTML;
+    let messages = document.getElementById('messages');
+    const html = Mustache.render(messageTemplateHtml,{
+        message
+    });
+    messages.insertAdjacentHTML('beforeend', html);
+});
+socket.on('disconnected', (value) => {
+    console.log(value);
+});
+let weatherInput = document.getElementById('weatherInput');
+let locationButton = document.getElementById('location');
+let button = document.getElementById('button');
+button.addEventListener('click', (e) => {
+    socket.emit('sendMessage', weatherInput.value);
+    weatherInput.value = "";
 })
-counter.addEventListener('click', () => {
-    socket.emit('valueChange',parseInt(paragraph.innerText))
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+socket.on('setLocation', (url) => {
+    let locationMessageTemplateHtml = document.getElementById('location-message-template').innerHTML;
+    let messages = document.getElementById('messages');
+    const html = Mustache.render(locationMessageTemplateHtml,{
+        url
+    });
+    messages.insertAdjacentHTML('beforeend', html);
+});
+
+
+locationButton.addEventListener('click', async () => {
+    locationButton.disabled = true;
+
+    console.log(navigator.geolocation);
+    if (!navigator.geolocation) {
+        return alert('geolocation is not supported for your browser');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        // socket.emit('USER:SEND_LOCATION', JSON.stringify(position));
+
+        // The issue with the position object/result that you get from calling navigator.geolocation.
+        // getCurrentPosition is that it's properties are not enumerable, meaning that you can't easily
+        //  "stringify"
+        // the object and pass it along with your socket.emit call, which is why you're getting an empty
+        //  object as a result.
+        // Although you can write a function that will extract these properties for you, you could do the 
+        // following:
+        const payload = {
+            coords: {
+                accuracy: position.coords.accuracy,
+                altitude: position.coords.altitude,
+                altitudeAccuracy: position.coords.altitudeAccuracy,
+                heading: position.coords.heading,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                speed: position.coords.speed
+            }
+        };
+        socket.emit('USER:SEND_LOCATION', JSON.stringify(payload), (message) => {
+            locationButton.disabled = false;
+        });
+    });
 });
--- src/index.js
+++ src/index.js
@@ -11,18 +11,33 @@ const server = http.createServer(app);
 const io = socketio(server);
 const directoryPath = path.join(__dirname, '../public');
 const port = process.env.port;
-let count=1;
+let count = 1;
 io.on('connection', (socket) => {
     console.log('websocket connection established');
-    socket.on('valueChange',(value)=>{
-        io.emit("countUpdated", value+1);
-    })
+    // socket.on('valueChange',(value)=>{
+    //     io.emit("countUpdated", value+1);
+    // })
+    socket.broadcast.emit('message', 'user joined');
+    socket.on('sendMessage', (cityName) => {
+        io.emit("sendCityNameToAllClients", cityName);
+    });
+    socket.on('disconnect', () => {
+        io.emit('message', 'user is disconnected');
+    });
+    socket.on('USER:SEND_LOCATION', (data, callback) => {
+
+        const payload = JSON.parse(data);
+        // socket.broadcast.emit('message', 'https://www.google.com/maps?q=' + payload.coords.latitude + "," + payload.coords.longitude);
+        io.emit('setLocation', 'https://www.google.com/maps?q=' + payload.coords.latitude + "," + payload.coords.longitude);
+        callback('Location shared');
+    });
+    socket.emit('message', "Welcome!");
 });
-app.use(express.static(directoryPath))
+app.use(express.static(directoryPath));
 // app.get('/', (req, res) => {
 //     res.sendFile(directoryPath);
 // });
-console.log('object')
+console.log('object');
 server.listen(port, () => {
     console.log('server is listening at', port);
 }
